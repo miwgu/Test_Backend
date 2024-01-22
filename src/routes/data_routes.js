2847a89158f5205e4e786e6408cf0b7ddc389923
Miwa G <miwa.guhres@hotmail.com>
2024-01-22 09:23:23 +0100

    Change code to->Admin fetch data but User cannot access data

--- src/routes/data_routes.js
+++ src/routes/data_routes.js
@@ -4,27 +4,29 @@ const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 //Use .env This file should not upload to github(.gitignore) This .env file is a secret file in our group5
-const secretKey = process.env.SECRET_KEY
+const secretKey = process.env.SECRET_KEY;
+console.log('SecretKey:', secretKey);
 
 router.get('/',(req,res) =>{
-  console.log('Imported secretKey:', secretKey);
-  
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
   console.log('Token:', token);
   
-  if(!token) return res.status(401).send('Acsedioder ss Denied');
+  if(!token) return res.status(401).send('Access Denied');
   
   try{
     const decoded_verified = jwt.verify(token, secretKey);
     console.log('Decoded JWT Payload:', decoded_verified);
     console.log('User Role:', decoded_verified.role);
     if(decoded_verified.role ==='admin'){
-      console.log('Data for admin');
-      res.json({data: 'Secret data for admin!'});
+      console.log('You are an Admin! You can access.');
+      //res.json({data: 'Secret data for admin!'});
+      res.status(200).json({data: 'Secret data for admin!'})
+    
     }else{
-      console.log('Data for user');
-      res.json({data: 'Secret data for user!'});
+      console.log('You are not Admin You cannot Access!');// You can see this in git Bash
+      //res.json({data: 'Secret data for user!'});
+      res.status(403).send("You are not an Admin! Access denied.")// If User is not "Admin", Send status 403 Because this data is for Admin 
     }
    }catch (error) {
       res.status(401).send(`Invalid Token: ${error.message}`);
